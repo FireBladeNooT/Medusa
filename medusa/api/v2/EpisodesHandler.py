3fcc82897c4f1cf60106a8c718957698fd0f1bcd
Fernando <fernandog@users.noreply.github.com>
2016-09-19 14:33:46 -0300

    Update EpisodesHandler.py

--- medusa/api/v2/EpisodesHandler.py
+++ medusa/api/v2/EpisodesHandler.py
@@ -1,5 +1,6 @@
 class EpisodeHandler():
-    def get(self, recipe_id=[]):
+    def get(self, recipe_id=None):
+        recipe_id = recipe_id or []
         if recipe_id == []:
             # Handle me
             self.set_status(400)
